7041691be05984186c643db8094de4fbcd38249c
mac <mac@Jade-MBP.net.fpt>
2018-11-25 10:36:47 +0700

    add first checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExampleCheckboxComponent } from './example-checkbox/example-checkbox.component';
+
+// For Material Modules
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleCheckboxComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/example-checkbox/example-checkbox.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+@Component({
+  selector: 'app-example-checkbox',
+  templateUrl: './example-checkbox.component.html',
+  styleUrls: ['./example-checkbox.component.css']
+})
+export class ExampleCheckboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
